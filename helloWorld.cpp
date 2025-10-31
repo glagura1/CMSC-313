8b01b782e344a0d7509bae0f273ede39c8410ec5
glagura1 <glagura1@umbc.edu>
2025-10-31 15:35:18 -0400

    Changing string output to say name and class

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "Hello teach. My name is Gio Lagura and I'm in your CMSC313 class" << std::endl;
+    
     return 0;
 }
